b2630cebf719b3f3387bb43e8be5e0c985e90308
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-03-15 16:52:14 -0400

    created class extension and properties

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,20 @@
 //this includes the vehicle class as a module
+const { Vehicle } = require("./vehicle");
 const VehicleModule = require("./vehicleBaseClass")
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+class Car extends Vehicle{
+    constructor(make, model, year, color, mileage, maximumPassengers, passengers, numberOfWheels, maximumSpeed, fuel, scheduleService){
+        super(make, model, year, color, mileage)
+        this.maximumPassengers = 5
+        this.passengers = 0
+        this.numberOfWheels = 4
+        this.maximumSpeed = 160 
+        this.fuel = 10
+        this.scheduleService = false
+    }
+
+}
